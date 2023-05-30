82c4e02c15f4e27b1781c3b4b1c9c50402774b92
caio <chss.bolsa99@gmail.com>
2023-05-30 13:48:13 -0300

    feature tarefa2

--- exceptions.py
+++ exceptions.py
@@ -2,10 +2,8 @@ from utils import data_processing
 from utils import ImpossibleTitlesError
 from utils import InvalidYearCupError
 from utils import NegativeTitlesError
-selection_info = {
-    "first_cup": "1932-10-18",
-    "titles": "4"
-}
+
+selection_info = {"first_cup": "1932-10-18", "titles": "4"}
 
 try:
     data_processing(selection_info)
