ee4bb08a725af24637ad5c0b0f92c2aba58e70a3
caio <chss.bolsa99@gmail.com>
2023-05-31 18:34:07 -0300

    removendo o venv

--- teams/urls.py
+++ teams/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 #from . import views
 
 from django.urls import path
-from .views import TeamAPIView
+from .views import TeamAPIView,TeamDetailView
 
 urlpatterns = [
     
     path('teams/', TeamAPIView.as_view()),
+    path('teams/<int:pk>/', TeamDetailView.as_view()),
 ]
--- teams/views.py
+++ teams/views.py
@@ -8,6 +8,10 @@ from exceptions import NegativeTitlesError
 from exceptions import ImpossibleTitlesError
 from utils import data_processing
 from django.forms.models import model_to_dict
+from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import NotFound
+from django.core.exceptions import ObjectDoesNotExist
+
 
 class TeamAPIView(APIView):
     def get(self, request):
@@ -45,3 +49,31 @@ class TeamAPIView(APIView):
             except ImpossibleTitlesError as e:
                 return Response({"error": e.message}, status=400)
         return Response(serializer.errors, status=400)
+    
+class TeamDetailView(APIView):
+    def get(self, request, pk):
+        try:
+            team = Team.objects.get(pk=pk)
+            serializer = TeamSerializer(team)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response({"message": "Team not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    def patch(self, request, pk):
+        try:
+            team = Team.objects.get(pk=pk)
+            serializer = TeamSerializer(team, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except ObjectDoesNotExist:
+            return Response({"message": "Team not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    def delete(self, request, pk):
+        try:
+            team = Team.objects.get(pk=pk)
+            team.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except ObjectDoesNotExist:
+            return Response({"message": "Team not found"}, status=status.HTTP_404_NOT_FOUND)
