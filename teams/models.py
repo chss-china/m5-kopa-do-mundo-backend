df1c3ce7deb8a312605cc6f1d7ac12ed9b06912b
caio <chss.bolsa99@gmail.com>
2023-06-01 10:47:03 -0300

    feat removendo comentarios

--- teams/models.py
+++ teams/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-# Create your models here.
+
 class Team(models.Model):
     name = models.CharField(max_length=30)
     titles = models.IntegerField(null=True,default=0)
